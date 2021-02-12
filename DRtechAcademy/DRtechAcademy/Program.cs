a8625c089a60bb2c8b0edb963009583dfd4fd6e5
Jaibean <58234317+Jaibean@users.noreply.github.com>
2021-02-12 15:30:14 -0500

    daily report
    assignment

---
+++ DRtechAcademy/DRtechAcademy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy \nStudent Daily Report");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string yourCurrentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int yourCurrentPage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string yourExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string yourFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            // creating new variable to convert
+            string hoursStudied = Console.ReadLine();
+            int yourHoursStudied = Convert.ToInt32(hoursStudied);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!” This is the end of the program.");
+            Console.ReadLine();
+
+    }
+    
+}

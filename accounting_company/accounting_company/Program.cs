ada08c18275370d95130223ec888b3e4a1d19512
Jaibean <jaimie.bertoli@gmail.com>
2021-02-12 13:20:07 -0500

    updates to Accounting Company
    added escaped quotes and line break

--- accounting_company/accounting_company/Program.cs
+++ accounting_company/accounting_company/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Acme Accounting Systems.");
+            Console.WriteLine("Welcome to Acme Accounting Systems. \n Remember, we're \"accounting\" on you!");
+            
             Console.ReadLine();
         }
     

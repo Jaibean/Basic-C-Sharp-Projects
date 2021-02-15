cc18c3fd8608d4cfea4179f2b05e9dae389b176f
Jaibean <jaimie.bertoli@gmail.com>
2021-02-15 18:44:31 -0500

    Boolean Logic Assigment

---
+++ CarInsuranceBoolLogicAssignment/CarInsuranceBoolLogicAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarInsuranceBoolLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Answer True or False.");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = yourAge >= 15 && DUI != true && speedingTickets <= 3;
+            Console.WriteLine(qualified);
+
+
+        }
+    }
+}

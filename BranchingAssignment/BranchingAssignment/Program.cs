0855d1f17f74217a10ec4f42b2c75f4c62abde0d
Jaibean <jaimie.bertoli@gmail.com>
2021-02-17 12:05:21 -0500

    branching assignment
    package if else program

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter the package weight:");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+            }
+            else 
+            {
+                Console.WriteLine("Please enter the package width:");
+                int packWidth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height:");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length:");
+                int packLength = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Your estimated total for shipping this package is: $528.00\nThank you!");
+
+            }
+
+        }
+    }
+}

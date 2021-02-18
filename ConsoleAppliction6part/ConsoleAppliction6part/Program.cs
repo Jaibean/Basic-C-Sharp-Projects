1ea730fe01b333ae43b79fc3c8adf835b31ce8ad
Jaibean <jaimie.bertoli@gmail.com>
2021-02-18 17:56:20 -0500

    6 part assignment

---
+++ ConsoleAppliction6part/ConsoleAppliction6part/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ConsoleAppliction6part
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // assignment part one
+
+            // one dimensional array of strings
+            string[] stringArray = { "Hello", "my", "name", "is", "jaimie" };
+
+            //// Ask the user to input some text.
+            //Console.WriteLine("What is your favorite type of weather?");
+            //string userInput = Console.ReadLine();
+
+            ////loop that iterates through each string in stringArray and adds userInput to the end of each string
+
+            //foreach (var i in stringArray)
+            //{
+            //    Console.WriteLine(i + " " + userInput);
+            //}
+
+            //// Assignemnt part two
+
+            ////infinite loop
+            //for (var j = 0; j < stringArray.Length; j--)
+            //{
+            //    Console.WriteLine("value : {0}", j);
+            //}
+            //Console.ReadLine();
+
+            //// Assignment part three
+
+            //// A loop where the comparison that’s used to determine whether
+            //// to continue iterating the loop is a “<” operator.
+
+            //for (int i = 0; i < stringArray.Length; i++)
+            //{
+            //    Console.Write(stringArray[i]);
+            //    if (i < stringArray.Length - 1)
+            //    {
+
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //// A loop where the comparison that’s used to determine whether
+            //// to continue iterating the loop is a “<=” operator.
+            
+            //foreach (var i in stringArray)
+            //if (stringArray.Length <= 5)
+            //{
+            //    Console.WriteLine("You are within the index");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are outside the arrays limitations");
+            //}
+            //Console.ReadLine();
+
+            // Assignment part four
+
+        }
+    }
+}
+   

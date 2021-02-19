f7f1b4cc643dd242a516a0155207ff3c6dd5834a
Jaibean <jaimie.bertoli@gmail.com>
2021-02-19 13:57:36 -0500

    branching update
    adding calculation and nested statement

--- BranchingAssignment/BranchingAssignment/Program.cs
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -9,14 +9,14 @@ namespace BranchingAssignment
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.ReadLine();
 
-            Console.WriteLine("Please enter the package weight:");
+            Console.WriteLine("Please enter your package weight:");
             int packWeight = Convert.ToInt32(Console.ReadLine());
 
             if (packWeight > 50)
             {
-                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-            else 
+            else
             {
                 Console.WriteLine("Please enter the package width:");
                 int packWidth = Convert.ToInt32(Console.ReadLine());
@@ -27,9 +27,20 @@ namespace BranchingAssignment
                 Console.WriteLine("Please enter the package length:");
                 int packLength = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Your estimated total for shipping this package is: $528.00\nThank you!");
+                int dimensions = packWidth * packLength * packHeight;
+                int price = (packWidth * packLength * packHeight * packWeight) / 100;
 
+                if (dimensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + price + "\nThank you!");
+                }
             }
+            Console.ReadLine();
 
         }
     }

dc2bf23d4bb6b70c8805496a0fd000266be89175
Jaibean <jaimie.bertoli@gmail.com>
2021-02-19 16:11:14 -0500

    string functions assisnment
     added in using System.Text; to enable StringBuilder. Completed assignment

--- StringFunctions/StringFunctions/Program.cs
+++ StringFunctions/StringFunctions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace StringFunctions
 {
@@ -6,6 +7,32 @@ namespace StringFunctions
     {
         static void Main(string[] args)
         {
+          
+            ////Assignment concatenate three strings and convert to upper case
+            //string firstString = "Jaimie went ";
+            //string secondString = "to the park ";
+            //string thirdString = "after dark.";
+
+            //string concat = firstString + secondString + thirdString;
+            //concat = concat.ToUpper();
+
+            //Console.WriteLine(concat);
+            //Console.ReadLine();
+
+            //Assignment create a Stringbuilder and build a paragraph of text on sentence at a time. 
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jaimie. ");
+            sb.Append("I am 26 years old. ");
+            sb.Append("I like to watch the snow fall.");
+            sb.Append("Luckily I do not have to shovel since I am in the city.");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+            // exercises
             //string name = "Jaimie";
             //string quote = "The man said, \"Hello\", Jaimie.\nHello on a new line\tHello on a tab.";
             //string fileName = @"C:\Users\Jaimie";
@@ -17,21 +44,12 @@ namespace StringFunctions
 
             //name = name.ToUpper();
 
-            //Console.WriteLine(quote);
-            //Console.ReadLine();
-
             //string name = "Jaimie";
             //name = "Joe";
 
             //Console.WriteLine(name);
             //Console.ReadLine();
 
-            StringBuilder sb = new StringBuilder();
-
-            sb.Append("My name is Jaimie");
-
-            Console.WriteLine(name);
-            Console.ReadLine();
         }
     }
 }

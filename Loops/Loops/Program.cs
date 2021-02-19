f885eba3de9e4d1358b23817177024510a05b9e9
Jaibean <jaimie.bertoli@gmail.com>
2021-02-19 14:30:08 -0500

    do while loop assignment
    updated there to be just a while loop. Fixed do-while loop

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -10,39 +10,77 @@ namespace Loops
             int number = Convert.ToInt32(Console.ReadLine());
             bool isGuessed = number == 12;
 
-            do
-            {
+            //// do while loop
+            //do
+            //{
+            //    switch (number)
+            //    {
 
+            //            case 62:
+            //                Console.WriteLine("Yoy guessed 62. Try again.");
+            //                Console.WriteLine("Guess a number?");
+            //                number = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 29:
+            //                Console.WriteLine("You guessed 29. Try again.");
+            //                Console.WriteLine("Guess a number?");
+            //                number = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 55:
+            //                Console.WriteLine("You guessed 55. Try again.");
+            //                Console.WriteLine("Guess a number?");
+            //                number = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 12:
+            //                Console.WriteLine("You guessed the number 12. That is correct!");
+            //                isGuessed = true;
+            //                break;
+            //            default:
+            //                Console.WriteLine("You are wrong, guess again.");
+            //                Console.WriteLine("What is your guess?");
+            //                number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        }
+            //    }
+            //     while (!isGuessed);
+
+            //     Console.Read();
+
+            //while loop
+
+            while (!isGuessed)
+            {
 
                 switch (number)
                 {
 
-                        case 62:
-                            Console.WriteLine("Yoy guessed 62. Try again.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 29:
-                            Console.WriteLine("You guessed 29. Try again.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 55:
-                            Console.WriteLine("You guessed 55. Try again.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 12:
-                            Console.WriteLine(" You guessed the number 12. That is correct!");
-                            break;
-                        default:
-                            Console.WriteLine("You are wrong.");
-                            break;
-                    }
+                    case 62:
+                        Console.WriteLine("Yoy guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong, guess again.");
+                        Console.WriteLine("What is your guess?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
                 }
-                 while (!isGuessed);
-
-                 Console.Read();
+            }
+            Console.Read();
         }
     }
 }

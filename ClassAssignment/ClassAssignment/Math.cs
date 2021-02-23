250bd8201110393a86521bcd799ed1d3db02cca6
Jaibean <jaimie.bertoli@gmail.com>
2021-02-23 15:43:20 -0500

    class assignment

---
+++ ClassAssignment/ClassAssignment/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ClassAssignment
+{
+    public class Math
+    {
+        public void MathOpp(int x, int y)
+        {
+            int result;
+            result = x * y;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

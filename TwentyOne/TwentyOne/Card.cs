c7fc6cd29dfc8a20445d63b37ba3601338e789f0
Jaibean <jaimie.bertoli@gmail.com>
2021-02-26 16:42:12 -0500

    Twenty One Game

--- TwentyOne/TwentyOne/Card.cs
+++ TwentyOne/TwentyOne/Card.cs
@@ -9,33 +9,38 @@ namespace TwentyOne
 {
     public struct Card
     {
-       
+
         public Suit Suit { get; set; }
         public Face Face { get; set; }
-      
-    }
-    public enum Suit
-    {
-        Clubs,
-        Diamonds,
-        Hearts,
-        Spades
-    }
-    public enum Face
-    {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King,
-        Ace
 
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Face, Suit);
+        }
+
+        public enum Suit
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades
+        }
+        public enum Face
+        {
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+
+        }
     }
 }
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,86 +11,32 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
 
-            Deck deck = new Deck();
+            Console.WriteLine("And how much did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
 
-            //working with lamba
-            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+            Console.WriteLine("Heelo, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
 
-            List<int> numberList = new List<int>() { 1, 2, 3, 4, 55, 666, 7 };
-
-            int sum = numberList.Sum(x => x + 5);
-            Console.WriteLine(sum);
-
-
-            //foreach (Card card in newList)
-            //{
-            //    Console.WriteLine(card.Face);
-            //}
-
-            //Deck deck = new Deck();
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
-            //Console.WriteLine(count);
-
-
-
-            ////working with Suit enum
-            //Card card = new Card();
-            //card.Suit = Suit.Clubs;
-            //int underlyingValue = Convert.ToInt32(Suit.Diamonds);
-            //Console.WriteLine(underlyingValue);
-
-
-            //Game game = new TwentyOneGame();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Jaimie";
-            //game += player;
-            //game -= player;
-
-            //List<Game> games = new List<Game>();
-            //TwentyOneGame game = new TwentyOneGame();
-            //games.Add(game);
-            //Game game = new TwentyOneGame();
-
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Jaimie", "Alex", "Mark" };
-            //game.ListPlayers();
-            //game.Play();
-            //Console.ReadLine();
-
-
-            //Card card1 = new Card();
-            //Card card2 = card1;
-            //card1.Face = Face.Eight;
-            //card2.Face = Face.King;
-
-            //Console.WriteLine(card1.Face);
-
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-         
-            Console.ReadLine();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                //polymorphism to expose overloaded operators we made
+                Game game = new TwentyOneGame();
+                game += player;
+                player.IsActivelyPlaying = true;
+                while (player.IsActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
         }
-
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i=0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
-
-   
-
 
     }
 

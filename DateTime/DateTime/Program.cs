2966d1eec41a5df04cd1a4036f0a8df2f6e325a3
Jaibean <jaimie.bertoli@gmail.com>
2021-03-01 15:25:04 -0500

    DateTime update

--- DateTime/DateTime/Program.cs
+++ DateTime/DateTime/Program.cs
@@ -20,8 +20,11 @@ namespace DateTimeAssignment
             int userInput = Convert.ToInt32(Console.ReadLine());
             // Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
 
-            TimeSpan thenAndNow = DateTime.Now - userInput;
-            Console.WriteLine("It will be " + thenAndNow + "hours untill " + userInput);
+
+
+
+            DateTime thenAndNow = DateTime.Now.AddHours(userInput);
+            Console.WriteLine("It will be " + thenAndNow + "in " + userInput + " hours.");
             Console.ReadLine();
 
 

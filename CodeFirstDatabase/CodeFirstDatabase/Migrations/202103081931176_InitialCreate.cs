11cf062137ecfd5d5283b433c8134559a62d8c96
Jaibean <58234317+Jaibean@users.noreply.github.com>
2021-03-08 14:44:16 -0500

    CodeFirst

---
+++ CodeFirstDatabase/CodeFirstDatabase/Migrations/202103081931176_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿namespace CodeFirstDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        PostId = c.Int(nullable: false, identity: true),
+                        StudentId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PostId)
+                .ForeignKey("dbo.Students", t => t.StudentId, cascadeDelete: true)
+                .Index(t => t.StudentId);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentId = c.Int(nullable: false, identity: true),
+                        StudentName = c.String(),
+                    })
+                .PrimaryKey(t => t.StudentId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Posts", "StudentId", "dbo.Students");
+            DropIndex("dbo.Posts", new[] { "StudentId" });
+            DropTable("dbo.Students");
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ CodeFirstDatabase/CodeFirstDatabase/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeFirstDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstDatabase.Program.StudentContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CodeFirstDatabase.Program+StudentContext";
+        }
+
+        protected override void Seed(CodeFirstDatabase.Program.StudentContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
